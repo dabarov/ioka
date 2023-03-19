aebeda80af6579dcbca1dfe3c5922d44f1fbfd9e
dabarov <simlyalde@gmail.com>
2023-03-19 20:00:28 +0600

    refactor: add group url in each class

--- ioka/api_resources/dashboard.py
+++ ioka/api_resources/dashboard.py
@@ -3,11 +3,12 @@ from ioka.api_resources.abstract.api_resource import APIResource
 
 
 class Dashboard(APIResource):
+    GROUP_API_URL = "/v2/dashboard/payments"
 
     @classmethod
     def search_payments(cls, **params):
-        return cls._get(f"{ioka.api_host}/v2/dashboard/payments", params=params)
+        return cls._get(f"{ioka.api_host}{cls.GROUP_API_URL}", params=params)
 
     @classmethod
     def export_payments(cls, **params):
-        return cls._get(f"{ioka.api_host}/v2/dashboard/payments/export", params=params)
+        return cls._get(f"{ioka.api_host}{cls.GROUP_API_URL}/export", params=params)
--- ioka/api_resources/payment.py
+++ ioka/api_resources/payment.py
@@ -3,11 +3,12 @@ from ioka.api_resources.abstract.api_resource import APIResource
 
 
 class Payment(APIResource):
+    GROUP_URL = "/v2/orders"
 
     @classmethod
     def retrieve(cls, order_id: str, payment_id: str, **params):
-        return cls._get(f"{ioka.api_host}/v2/orders/{order_id}/payments/{payment_id}", params=params)
+        return cls._get(f"{ioka.api_host}{cls.GROUP_URL}/{order_id}/payments/{payment_id}", params=params)
 
     @classmethod
     def create_card(cls, order_id: str, **kwargs):
-        return cls._post(f"{ioka.api_host}/v2/orders/{order_id}/payments/card", json_obj=kwargs)
+        return cls._post(f"{ioka.api_host}{cls.GROUP_URL}/{order_id}/payments/card", json_obj=kwargs)
