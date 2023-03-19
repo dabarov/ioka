0ae75824618108e47b4146b8fa02c793455bb8e1
dabarov <simlyalde@gmail.com>
2023-03-19 19:28:56 +0600

    feat(order): add order methods

--- ioka/api_resources/order.py
+++ ioka/api_resources/order.py
@@ -1,12 +1,41 @@
-import requests
-
 import ioka
+from ioka.api_resources.abstract.api_resource import APIResource
 
 
-class Order:
+class Order(APIResource):
 
     @classmethod
-    def list(cls):
-        headers = {"API-KEY": ioka.api_key}
-        response = requests.get(f"{ioka.api_host}/v2/orders", headers=headers)
-        return response.text
+    def list(cls, **params):
+        return cls.get(f"{ioka.api_host}/v2/orders", params=params)
+
+    @classmethod
+    def retrieve(cls, order_id: str, **params):
+        return cls.get(f"{ioka.api_host}/v2/orders/{order_id}", params=params)
+
+    @classmethod
+    def list_events(cls, order_id: str, **params):
+        return cls.get(f"{ioka.api_host}/v2/orders/{order_id}/events", params=params)
+
+    @classmethod
+    def list_refunds(cls, order_id: str, **params):
+        return cls.get(f"{ioka.api_host}/v2/orders/{order_id}/refunds", params=params)
+
+    @classmethod
+    def retrieve_refund(cls, order_id: str, refund_id: str, **params):
+        return cls.get(f"{ioka.api_host}/v2/orders/{order_id}/refunds/{refund_id}", params=params)
+
+    @classmethod
+    def create(cls, **kwargs):
+        return cls.post(f"{ioka.api_host}/v2/orders", json_obj=kwargs)
+
+    @classmethod
+    def capture(cls, order_id: str, **kwargs):
+        return cls.post(f"{ioka.api_host}/v2/orders/{order_id}/capture", json_obj=kwargs)
+
+    @classmethod
+    def cancel(cls, order_id: str, **kwargs):
+        return cls.post(f"{ioka.api_host}/v2/orders/{order_id}/cancel", json_obj=kwargs)
+
+    @classmethod
+    def refund(cls, order_id: str, **kwargs):
+        return cls.post(f"{ioka.api_host}/v2/orders/{order_id}/refund", json_obj=kwargs)
